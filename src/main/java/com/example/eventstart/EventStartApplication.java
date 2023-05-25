da3ff1048817f00bf39d5d3fe4ac01b6995eef49
JxxHxxx <leesin5498@naver.com>
2023-05-25 14:54:58 +0900

    Initial commit

---
+++ src/main/java/com/example/eventstart/EventStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventstart/EventStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
