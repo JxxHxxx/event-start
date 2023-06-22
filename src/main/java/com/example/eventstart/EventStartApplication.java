ceccbb3f9255a144e190f9271715c42dfd33a81a
JxxHxxx <leesin5498@naver.com>
2023-06-22 22:43:53 +0900

    스프링 이벤트 학습을 위해 기존 코드 모두 삭제

--- src/main/java/com/example/eventstart/EventStartApplication.java
+++ src/main/java/com/example/eventstart/EventStartApplication.java
@@ -1,6 +1,5 @@
 package com.example.eventstart;
 
-import com.example.eventstart.v2.repository.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +8,6 @@ public class EventStartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventStartApplication.class, args);
-
-        MemberRepository memberRepository = new MemberRepository();
-        memberRepository.save("유니");
-        memberRepository.save("허니");
     }
 
 }
